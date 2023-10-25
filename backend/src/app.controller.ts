20be24e78983173ef15b766fda0d2eab152920bb
Lucasteisouza <lucasteisouza@gmail.com>
2023-10-25 16:31:20 -0300

    inicia desenvolvimento do backend base. Cria controller para rota login --implementação do service pendente

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { Body } from '@nestjs/common';
 import { IUser } from './interfaces/';
@@ -8,7 +9,16 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('login')
-  login(@Body() body: IUser) {
-    return this.appService.login(body);
+  @ApiResponse({ status: 200, description: 'Login realizado com sucesso.' })
+  @ApiResponse({ status: 401, description: 'Usuário ou senha inválidos.' })
+  async login(@Body() body: IUser) {
+    try {
+      const response = await this.appService.login(body);
+      if (response) {
+        return response;
+      }
+    } catch (error) {
+      throw new Error('Erro no login');
+    }
   }
 }
